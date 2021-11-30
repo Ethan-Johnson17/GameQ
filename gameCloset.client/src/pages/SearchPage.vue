27ecbd3d49cea4dab836e2449af3d94b1757e20f
Tristan Johns <tfj@protonmail.com>
2021-11-30 13:51:03 -0700

    search

--- gameCloset.client/src/pages/SearchPage.vue
+++ gameCloset.client/src/pages/SearchPage.vue
@@ -1,22 +1,62 @@
 <template>
-  <div class="search"></div>
+  <div class="search">
+    <div class="mb-3 w-25">
+      <form @submit.prevent="searchGames">
+        <label for="" class="form-label"></label>
+        <input
+          v-model="search"
+          type="text"
+          class="form-control"
+          name=""
+          id=""
+          aria-describedby="helpId"
+          placeholder="Search Games"
+        />
+        <input
+          name="search"
+          id=""
+          class="btn btn-primary"
+          type="submit"
+          value=""
+        />
+      </form>
+    </div>
+  </div>
 </template>
 
 
 <script>
+import { computed, onMounted, ref } from "@vue/runtime-core"
+import { gamesService } from "../services/GamesService"
+import { logger } from "../utils/Logger"
+import { AppState } from "../AppState"
 export default {
   setup() {
-    onMounted(async () => {
+    const search = ref('')
+    // onMounted(async () => {
+    //   try {
+    //     if (AppState.atlasGames.length > 0) { return }
+    //     else { await gamesService.getAll('/search?q=' + search) }
+    //     if there's nothing in the appstate, call atlasApi
+    //   } catch (error) {
+    //     logger.error(error)
+    //   }
+    // })
 
-      try {
-        await gamesService.getAll('/search?q=')
-      } catch (error) {
-        logger.error(error)
-      }
-    })
     return {
+      search,
       user: computed(() => AppState.user),
+      atlasGames: computed(() => AppState.atlasGames),
 
+      async searchGames() {
+        try {
+          // await gamesService.getAll('/search?q=' + search.value)
+          logger.log(search.value)
+          search.value = ''
+        } catch (error) {
+          logger.error
+        }
+      }
     }
   }
 }
--- gameCloset.client/src/services/GamesService.js
+++ gameCloset.client/src/services/GamesService.js
@@ -1,4 +1,5 @@
 import { AppState } from "../AppState"
+import { Game } from "../Models/Game"
 import { logger } from "../utils/Logger"
 import { atlasApi } from "./AxiosService"
 const clientId = '&client_id=RhH4WBOfK3'
@@ -6,8 +7,9 @@ const clientId = '&client_id=RhH4WBOfK3'
 class GamesService {
   async getAll(query = '') {
     const res = await atlasApi.get(query + clientId)
-    logger.log(res)
-    AppState.atlasGames.map(g => new Game(g))
+    logger.log('res ', res)
+    AppState.atlasGames = res.data.games.map(g => new Game(g))
+    logger.log('AppState', AppState.atlasGames)
   }
 }
 export const gamesService = new GamesService()
