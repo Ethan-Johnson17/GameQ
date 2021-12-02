720ab2a01050b7f92bb2ab7e68541ea043617f4e
Scott Bickish <sbickish3@gmail.com>
2021-12-02 13:49:55 -0700

    votes notes added

--- gameCloset/server/services/GamesQueueService.js
+++ gameCloset/server/services/GamesQueueService.js
@@ -21,14 +21,20 @@ class GamesQueueService {
   }
 
   async edit(body) {
+    // NOTE we first have to find the accountId
     const gameq = await this.getById(body.id)
+    // NOTE here we are setting votes to the accountId's votes if any
     const votes = gameq.votes
+    // STUB here we are assigning index to the actual index of the votes(schema) array that matches your own accountId
     const index = votes.findIndex(v => v.toString() === body.accountId)
+    // SECTION the if statement is checking if it exists. -1 means it doesnt so it will then push it to the array
     if (index === -1) {
       votes.push(body.accountId)
+      // STUB if it does find your id in the array it will take it out thus preventing you from voting multiple times
     } else {
       votes.splice(index, 1)
     }
+    // REVIEW here it will save anytime it changes wether it adds or removes then return you the new array
     await gameq.save()
     return gameq
   }
