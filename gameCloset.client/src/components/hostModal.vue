f7c438681e2dcbb78ea5f03abc7bdef2bd1aa8f1
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2021-12-03 10:22:09 -0700

    Search bug fixed

---
+++ gameCloset.client/src/components/hostModal.vue
@@ -0,0 +1,147 @@
+<template>
+  <div
+    class="modal fade"
+    id="hostModal"
+    aria-hidden="true"
+    aria-labelledby="hostModal"
+    tabindex="-1"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalToggleLabel">
+            Host a Game Night
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <form @submit.prevent="createGameNight">
+          <div class="modal-body">
+            <div class="row">
+              <div class="col m-2">
+                <input
+                  type="text"
+                  aria-label="name"
+                  placeholder="Name your game night"
+                  class="form-control"
+                  v-model="state.editable.name"
+                  required
+                />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col m-2">
+                <input
+                  type="text"
+                  aria-label="location"
+                  placeholder="Location"
+                  class="form-control"
+                  v-model="state.editable.location"
+                  required
+                />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col m-2">
+                <input
+                  type="datetime-local"
+                  aria-label="date"
+                  placeholder="Date"
+                  class="form-control"
+                  v-model="state.editable.date"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="submit"
+              class="btn btn-primary"
+              data-bs-target="#exampleModalToggle2"
+              data-bs-toggle="modal"
+              data-bs-dismiss="modal"
+            >
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div
+    class="modal fade"
+    id="exampleModalToggle2"
+    aria-hidden="true"
+    aria-labelledby="exampleModalToggleLabel2"
+    tabindex="-1"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          Hide this modal and show the first with the button below.
+        </div>
+        <div class="modal-footer">
+          <button
+            class="btn btn-primary"
+            data-bs-target="#exampleModalToggle2"
+            data-bs-toggle="modal"
+            data-bs-dismiss="modal"
+          >
+            Add Games
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { reactive, computed, ref } from "@vue/reactivity";
+import { logger } from "../utils/Logger";
+import Pop from "../utils/Pop";
+import { useRoute, useRouter } from "vue-router";
+import { AppState } from "../AppState";
+
+export default {
+  setup() {
+    const newPlayer = ref({})
+    const state = reactive({
+      editable: {},
+    });
+    return {
+      state,
+
+      async createGameNight() {
+        logger.log('create', state.editable)
+        await gameNightService.createGameNight(state.editable)
+        // const eventId = AppState.activeEvent.id
+        // router.push({
+        //   name: "EventDetails",
+        //   params: { id: eventId }
+        // })
+        newPlayer.value.accountId = AppState.account.id
+        newPlayer.value.gameNightId = route.params.id
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
