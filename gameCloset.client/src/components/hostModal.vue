99596de4f7e8d1286cfab181df48fcea539110e9
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2021-12-03 13:54:40 -0700

    2nd modal

--- gameCloset.client/src/components/hostModal.vue
+++ gameCloset.client/src/components/hostModal.vue
@@ -30,6 +30,27 @@
             </div>
           </div>
 
+
+
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
+              data-bs-dismiss="modal">
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
+    tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
           <!-- NOTE This is the game choosing drop down  -->
           <div class="row">
             <div class="col">
@@ -69,51 +90,16 @@
               </li>
             </ul>
           </div>
-
-          <div class="modal-footer">
-            <button type="submit" class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
-              data-bs-dismiss="modal">
-              Create
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-  <!-- <div
-    class="modal fade"
-    id="exampleModalToggle2"
-    aria-hidden="true"
-    aria-labelledby="exampleModalToggleLabel2"
-    tabindex="-1"
-  >
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          Hide this modal and show the first with the button below.
         </div>
         <div class="modal-footer">
-          <button
-            class="btn btn-primary"
-            data-bs-target="#exampleModalToggle2"
-            data-bs-toggle="modal"
-            data-bs-dismiss="modal"
-          >
+          <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
+            data-bs-dismiss="modal">
             Add Games
           </button>
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
 </template>
 
 
