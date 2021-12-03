61489d2e99c32aa69253b5549e6a0983b967197f
Tristan Johns <tfj@protonmail.com>
2021-12-03 13:56:22 -0700

    modal work in progress

--- gameCloset.client/src/components/hostModal.vue
+++ gameCloset.client/src/components/hostModal.vue
@@ -1,31 +1,60 @@
 <template>
-  <div class="modal fade" id="hostModal" aria-hidden="true" aria-labelledby="hostModal" tabindex="-1">
+  <div
+    class="modal fade"
+    id="hostModal"
+    aria-hidden="true"
+    aria-labelledby="hostModal"
+    tabindex="-1"
+  >
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header bg-secondary">
           <h5 class="modal-title" id="exampleModalToggleLabel">
             Host a Game Night
           </h5>
-          <button type="button" class="btn-close bg-secondary" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close bg-secondary"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <form @submit.prevent="createGameNight">
           <div class="modal-body">
             <div class="row">
               <div class="col m-2">
-                <input type="text" aria-label="name" placeholder="Name your game night" class="form-control"
-                  v-model="state.editable.name" required />
+                <input
+                  type="text"
+                  aria-label="name"
+                  placeholder="Name your game night"
+                  class="form-control"
+                  v-model="state.editable.name"
+                  required
+                />
               </div>
             </div>
             <div class="row">
               <div class="col m-2">
-                <input type="text" aria-label="location" placeholder="Location" class="form-control"
-                  v-model="state.editable.location" required />
+                <input
+                  type="text"
+                  aria-label="location"
+                  placeholder="Location"
+                  class="form-control"
+                  v-model="state.editable.location"
+                  required
+                />
               </div>
             </div>
             <div class="row">
               <div class="col m-2">
-                <input type="datetime-local" aria-label="date" placeholder="Date" class="form-control"
-                  v-model="state.editable.gameNightDate" required />
+                <input
+                  type="datetime-local"
+                  aria-label="date"
+                  placeholder="Date"
+                  class="form-control"
+                  v-model="state.editable.gameNightDate"
+                  required
+                />
               </div>
             </div>
           </div>
@@ -37,42 +66,77 @@
             </div>
           </div>
           <div class="dropdown mx-4 my-2">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Choose Game 1
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {{ state.selected1 }}
             </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
               <li v-for="game in closetGames" :key="game.atlasGameId">
-                <div class="dropdown-item" @click="selected1= '{{game.name}}'">{{ game.name }}</div>
+                <div
+                  class="dropdown-item selectable"
+                  @click="state.selected1 = '{{game.name}}'"
+                >
+                  {{ game.name }}
+                </div>
               </li>
             </ul>
           </div>
           <div class="dropdown mx-4 my-2">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Choose Game 2
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton2"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {{ state.selected2 }}
             </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
               <li v-for="game in closetGames" :key="game.atlasGameId">
-                <div class="dropdown-item" @click="selected2= '{{game.name}}'">{{ game.name }}</div>
+                <div
+                  class="dropdown-item selectable"
+                  @click="state.selected2 = '{{game.name}}'"
+                >
+                  {{ game.name }}
+                </div>
               </li>
             </ul>
           </div>
           <div class="dropdown mx-4 my-2">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton3"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Choose Game 3
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton3"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {{ state.selected3 }}
             </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton3">
               <li v-for="game in closetGames" :key="game.atlasGameId">
-                <div class="dropdown-item" @click="selected3= '{{game.name}}'">{{ game.name }}</div>
+                <div
+                  class="dropdown-item selectable"
+                  @click="state.selected3 = '{{game.name}}'"
+                >
+                  {{ game.name }}
+                </div>
               </li>
             </ul>
           </div>
 
           <div class="modal-footer">
-            <button type="submit" class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
-              data-bs-dismiss="modal">
+            <button
+              type="submit"
+              class="btn btn-primary"
+              data-bs-target="#exampleModalToggle2"
+              data-bs-toggle="modal"
+              data-bs-dismiss="modal"
+            >
               Create
             </button>
           </div>
@@ -118,47 +182,47 @@
 
 
 <script>
-  import { reactive, computed, ref } from "@vue/reactivity";
-  import { logger } from "../utils/Logger";
-  import Pop from "../utils/Pop";
-  import { useRoute, useRouter } from "vue-router";
-  import { AppState } from "../AppState";
-  import { gameNightService } from "../services/GameNightService";
+import { reactive, computed, ref } from "@vue/reactivity";
+import { logger } from "../utils/Logger";
+import Pop from "../utils/Pop";
+import { useRoute, useRouter } from "vue-router";
+import { AppState } from "../AppState";
+import { gameNightService } from "../services/GameNightService";
 
 
 
 
-  export default {
-    setup() {
-      const newPlayer = ref({})
-      const state = reactive({
-        editable: {},
-        selected1: '',
-        selected2: '',
-        selected3: '',
-      });
-      return {
-        closetGames: computed(() => AppState.myGames.filter(g => g.owned)),
-        state,
+export default {
+  setup() {
+    const newPlayer = ref({})
+    const state = reactive({
+      editable: {},
+      selected1: 'Select a game:',
+      selected2: 'Select a game:',
+      selected3: 'Select a game:',
+    });
+    return {
+      closetGames: computed(() => AppState.myGames.filter(g => g.owned)),
+      state,
 
 
 
 
 
-        async createGameNight() {
-          logger.log('create', state.editable)
-          await gameNightService.createGameNight(state.editable)
-          // const gameId = AppState.activeGame.id
-          // router.push({
-          //   name: "active",
-          //   params: { id: gameId }
-          // })
-          // newPlayer.value.accountId = AppState.account.id
-          // newPlayer.value.gameNightId = route.params.id
-        }
+      async createGameNight() {
+        logger.log('create', state.editable)
+        await gameNightService.createGameNight(state.editable)
+        // const gameId = AppState.activeGame.id
+        // router.push({
+        //   name: "active",
+        //   params: { id: gameId }
+        // })
+        // newPlayer.value.accountId = AppState.account.id
+        // newPlayer.value.gameNightId = route.params.id
       }
     }
   }
+}
 </script>
 
 
