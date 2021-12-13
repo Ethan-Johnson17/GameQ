40bd040125ea0fcb9600f47902814b63675b5437
Kylee <kylee.j.marlin@gmail.com>
2021-12-13 13:40:35 -0700

    update

---
+++ gameCloset/client/assets/AboutPage.96d87afd.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.304b9ef4.js";import{o,b as t,d as s}from"./vendor.4a4b261e.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,u,f,b){return o(),t("div",n,c)}var m=e(a,[["render",_]]);export{m as default};
